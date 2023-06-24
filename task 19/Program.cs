827cbcfb6925f4df37f80895938171d7b901a3b3
Питенко София <pitenko.sofiya@mail.ru>
2023-06-25 00:24:46 +0300

    Add project files.

---
+++ task 19/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = multidimensional.Generatot(5,1,10);
+            for (int i = 0;i < c.GetLength(0); i++) 
+            {
+                for (int j = 0; j < c.GetLength(1); j++)
+                {
+                    Console.Write($"{c[i,j]} ");
+
+                }
+                Console.WriteLine();
+            }
+
+            var x = multidimensional.NeighboringLargest(c);
+
+            Console.WriteLine(x);
+           Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ task 19/multidimensional.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_19
+{
+    internal class multidimensional
+    {
+        public static int[,] Generatot(int n, int min, int max)
+        {
+            int[,] array = new int[n, n];
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = random.Next(min, max);
+                }
+            }
+            return array;
+        }
+
+        public static int MinElement(int[,] array)
+        {
+            int min = array[0, 0];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (min > array[i, j])
+                    {
+                        min = array[i, j];
+                    }
+                }
+            }
+            return min;
+        }
+
+        public static int MaxElement(int[,] array)
+        {
+            int max = array[0, 0];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (max < array[i, j])
+                    {
+                        max = array[i, j];
+                    }
+                }
+            }
+            return max;
+        }
+
+        public static string MinIndex(int[,] array)
+        {
+            int min = array[0, 0];
+            string index = "";
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (min > array[i, j])
+                    {
+                        min = array[i, j];
+                        index = $"{i} {j}";
+                    }
+                }
+            }
+            return index;
+        }
+
+        public static void Print(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static string MaxIndex(int[,] array)
+        {
+            int max = array[0, 0];
+            string index = "";
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (max < array[i, j])
+                    {
+                        max = array[i, j];
+                        index = $"{i} {j}";
+                    }
+                }
+            }
+            return index;
+        }
+
+        public static int NeighboringLargest(int[,] array)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLongLength(1); j++)
+                {
+                    if (j == 0 && i == array.GetLength(0) - 1)
+                    {
+                        count++;
+                        continue;
+                    }
+
+                    if (j == 0)
+                    {
+                        if (array[i, j] > array[i + 1, j])
+                        {
+                            count++;
+                        }
+
+                        continue;
+                    }
+
+                    if (i == array.GetLength(0) - 1)
+                    {
+                        if (array[i, j] > array[i, j - 1])
+                        {
+                            count++;
+                        }
+
+                        continue;
+                    }
+
+                    if (array[i, j] > array[i + 1, j])
+                    {
+                        if (array[i, j] > array[j, j - 1])
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
